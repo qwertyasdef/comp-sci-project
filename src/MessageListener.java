086f286cdfe126fb799ee638e22838304d4d29e2
qwertyasdef <byao9310@gmail.com>
2018-06-20 10:35:03 -0700

    Add files via upload

--- src/MessageListener.java
+++ src/MessageListener.java
@@ -3,21 +3,24 @@ import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.QUESTION_MESSAGE;
 import static javax.swing.JOptionPane.YES_NO_OPTION;
 import static javax.swing.JOptionPane.YES_OPTION;
 
 public class MessageListener {
     
     private MessageAnalyzer parent;
-    private VoltageRatioInput sensor = null;
+    private VoltageRatioInput sensor;
     private boolean simulate = askSimulate();
     private SimulateFrame simulation;
+    private int serial;
+    private int channel;
     
     Timer timer;
     ArrayList<Double> input = new ArrayList<>();
     double min = -1;
     double max = -1;
-    int samplePeriod = 20; // Read sensor every this many ms
+    int samplePeriod = 50; // Read sensor every this many ms
     int msgEndWait = 5; // Seconds to wait to make sure message is over
     
     public MessageListener(MessageAnalyzer parent) {
@@ -31,20 +34,26 @@ public class MessageListener {
         this.startTimer();
     }
 
-    public boolean askSimulate() {
+    private boolean askSimulate() {
         int result = JOptionPane.showConfirmDialog(null, "Simulate sensors?", "Simulate?", YES_NO_OPTION);
         return (result == YES_OPTION);
+    }
+    
+    private void getSerial() {
+        this.serial = Integer.parseInt(JOptionPane.showInputDialog(null, "What is the serial number of the phidget?", "Serial number", QUESTION_MESSAGE));
+        this.channel = Integer.parseInt(JOptionPane.showInputDialog(null, "What channel is your sensor in?", "Channel", QUESTION_MESSAGE));
     }
     
     public void setUpIK() {
         if (sensor!=null)
             return;
         
+        this.getSerial();
         try {
                 sensor=new VoltageRatioInput();
                 System.out.println("Opening and waiting 3 seconds for sensor attatchment...");
-                sensor.setDeviceSerialNumber(35663);
-                sensor.setChannel(0);
+                sensor.setDeviceSerialNumber(serial);
+                sensor.setChannel(channel);
                 sensor.open(3000);
                 System.out.println("vr1 value is : " + sensor.getSensorValue() );
         } catch(Exception e) {
@@ -58,6 +67,7 @@ public class MessageListener {
             return simulation.getReading();
         } else {
             try {
+                System.out.println(sensor.getVoltageRatio());
                 return sensor.getVoltageRatio();
             } catch (PhidgetException e) {
                 return -1;
